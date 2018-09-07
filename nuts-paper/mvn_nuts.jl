2bdebe18d3a4b1d3c362dec6f56a922b5a68cfd9
Hong Ge <hg344@cam.ac.uk>
2018-09-07 18:20:45 +0100

    Update mvn_nuts.jl

--- nuts-paper/mvn_nuts.jl
+++ nuts-paper/mvn_nuts.jl
@@ -2,13 +2,13 @@ using Distributions
 using Turing
 
 # 250-dimensional multivariate normal (MVN)
-@model mvn_nuts(A) = begin
-  dim, _ = size(A)
-  Θ ~ MvNormal(zeros(dim), A)
+@model mvn_nuts(A, d) = begin
+  Θ ~ d
 end
 
 # Sample a precision matrix A from a Wishart distribution
 # with identity scale matrix and 250 degrees of freedome
-A = rand(Wishart(250, Matrix{Float64}(I, 250, 250)))
-
-chain = sample(mvn_nuts(A), NUTS(1000, 0.65))
+dim = 250
+A   = rand(Wishart(250, Matrix{Float64}(I, 250, 250)))
+d   = MvNormal(zeros(250), A)
+chain = sample(mvn_nuts(A, d), NUTS(1000, 0.65))
