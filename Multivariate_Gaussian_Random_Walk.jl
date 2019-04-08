f9b9a00c1d35e290283bfdb0bd2c9fd8ebb0fc2e
Ritesh Kumar <ritesh99rakesh@gmail.com>
2019-04-08 16:01:06 +0530

    added multivariate gaussian random walk example

---
+++ Multivariate_Gaussian_Random_Walk.jl
@@ -0,0 +1,53 @@
+# Ported from
+
+# Import packages
+
+using Turing, Distributions, MCMCChains
+using LinearAlgebra, Plots
+
+using Random
+Random.seed!(12);
+
+Turing.turnprogress(false);
+
+
+# Define cumulative sum function
+
+function cumsum(X)
+    N, D = size(X)
+    for j in 2:D
+        X[:,j] = X[:, j] + X[:, j-1]
+    end
+    return X
+end
+
+
+# Generating data
+
+D = 3
+N = 300
+sections = 5
+period = div(N, sections)
+
+Sigma_a = randn(D, D)
+Sigma_a = transpose(Sigma_a)*Sigma_a
+L_a = cholesky(Sigma_a).L
+
+Sigma_b = randn(D, D)
+Sigma_b = transpose(Sigma_b)*Sigma_b
+L_b = cholesky(Sigma_b).L
+
+# Gaussian Random Walk
+alpha = transpose(cumsum(L_a*randn(D, sections)))
+beta = transpose(cumsum(L_b*randn(D, sections)))
+sigma = 0.1
+
+t = reshape(Vector(collect(0:N-1))/N, (300, 1))
+alpha = repeat(alpha, period)
+beta = repeat(beta, period)
+y = alpha + beta.*t .+ sigma*randn(N, 1);
+
+
+# Plot the data
+
+plot(t, y)
