20953aae1882fd5bb911838f1d2f6be7e1d169f0
saumyagshah <sshah@iitk.ac.in>
2019-08-27 11:37:07 +0530

    Improved ARIMA implementation post blog

--- time_series_ARIMA.jl
+++ time_series_ARIMA.jl
@@ -1,6 +1,6 @@
 
-#Import Turing, Distributions, StatsBase, DataFrames and CSV
-using Turing, Distributions, StatsBase, DataFrames, CSV
+# Import Turing, Distributions, StatsBase, DataFrames and CSV, HypothesisTests and LinearAlgebra
+using Turing, Distributions, StatsBase, DataFrames, CSV, HypothesisTests, LinearAlgebra
 
 # Import MCMCChain, Plots and StatsPlots
 using MCMCChains, Plots, StatsPlots
@@ -14,98 +14,137 @@ Turing.turnprogress(false)
 
 # Load in the dataset downloaded from https://github.com/inertia7/timeSeries_sp500_R/blob/master/data/data_master_1.csv
 #MIT License
-df = CSV.read("../data/DowJones.csv")
+println("Loading the dataset")
+df = CSV.read("../data/data_master_1.csv")
 df[:sp_500]
-# plot(df[:Date], df[Symbol("Adj Close")])
 
-s = Float64[]
-for ele in df[:sp_500]
-# for ele in df[Symbol("Adj Close")]
-    push!(s, ele)
-end
+s = df.sp_500
 pyplot()
-plot(s)
+plot(s, reuse = false, title = "Plot of the complete data")
+gui()
 
-# Split into training and test sets. We will predict for the next 4 days using the data from the past 32 days
+# Split into training and test sets. 
+println("Split into training and test sets. We will predict for the next 4 days using the data from the past 32 days")
 train_percentage = 0.95
 s_train = s[1:floor(Int, train_percentage*length(s))]
 N = length(s_train)
 
 # Plot the training data
-plot(s_train)
+println("Plot the training data")
+plot(s_train, reuse = false, title = "Plot of the training data")
+gui()
 
+ADFTest(s_train, Symbol("constant"), 5)
+
+println("Plot the differenced series")
 s_diff = diff(s_train)
-N_diff = length(s_diff)
-plot(s_diff)
+plot(s_diff, reuse = false, title = "Plot of the differenced series")
+gui()
+
+ADFTest(s_diff, Symbol("constant"), 5)
 
 #Plot ACF and PACF plots
-total_lags = 10
-s1 = scatter(collect(1:total_lags), autocor(s_diff, collect(1:total_lags)), title = "ACF")
-s2 = scatter(collect(1:total_lags), pacf(s_diff, collect(1:total_lags)), title = "PACF")
-plot(s1, s2, layout = (2, 1))
-# The PACF plot cuts off at k = 2, so we will have an AR(2) model for this dataset.
+println("Plotting ACF and PACF plots")
+total_lags = 20
+s1 = scatter(collect(1:total_lags), autocor(s_diff, collect(1:total_lags)), title = "ACF", ylim = [-0.3,0.5])
+s2 = scatter(collect(1:total_lags), pacf(s_diff, collect(1:total_lags)), title = "PACF", ylim = [-0.3,0.5])
+plot(s1, s2, layout = (2, 1), reuse = false)
+gui()
+println("From the ACF and PACF plots, both ARIMA(0, 1, 1) and ARIMA(1, 1, 0) seem plausible")
 
-#Defining the model
-σ = 1
-@model ARIMA110(x, N) = begin
-    beta_1 ~ Uniform(-1, 1)
-    for t in 3:N
-        μ = rand(Normal(0, 1)) + beta_1 * x[t-1] 
-        x[t] ~ Normal(μ, 1) 
+@model ARIMA110(x) = begin
+    T = length(x)
+    μ ~ Uniform(-10, 10)
+    ϕ ~ Uniform(-1, 1)
+    for t in 3:T
+        val = μ +                      # Drift term.
+              x[t-1] +                 # ARIMA(0,1,0) portion.
+              ϕ * (x[t-1] - x[t-2]) # ARIMA(1,0,0) portion.
+        x[t] ~ Normal(val, 1)
     end
-end;
-
-# This is temporary while the reverse differentiation backend is being improved.
-Turing.setadbackend(:forward_diff)
-
-# Sample using NUTS
-chain_ARIMA110 = sample(ARIMA110(s_diff, N_diff), NUTS(500, 200, 0.65) )
-
-plot(chain_ARIMA110)
-
-#Define the model
-
-σ = 1
-@model ARIMA011(x, N) = begin
-    beta_1 ~ Uniform(-1, 1)
-    μ ~ Uniform(0, 10)
-    for t in 3:N
-        val = μ + rand(Normal(0,σ)) + beta_1 * rand(Normal(0, 1)) 
-        x[t] ~ Normal(val, 1) 
-    end
-end;
-
-# This is temporary while the reverse differentiation backend is being improved.
-Turing.setadbackend(:forward_diff)
-
-# Sampling using NUTS
-chain_ARIMA011 = sample(ARIMA011(s_diff, N_diff), NUTS(500, 200, 0.65) )
-
-plot(chain_ARIMA011)
-
-# ARIMA 011 WAIC calculation
-
-sampling_size = 500
-lpppd = 0.0
-p = 0.0
-for i = 1:N
-    likelihood = Float64[]
-    for sample = 1:sampling_size
-        beta_1_value = chain_ARIMA011[:beta_1][sample] 
-#         mu_value = chain_ARIMA011[:mu][sample,i,:]
-#         dist = Normal(mu_value + eta_value[1]*tau_value, sigma[i])
-        push!(likelihood, pdf(dist, y[i]) )
-    end
-    
-    # Adding the contribution of the current observation in lpppd
-    log_mean = log(mean(likelihood))
-    lpppd += log_mean
-    
-    #Calculating the p_waic value
-    log_likelihood = log.(likelihood)
-    var_likelihood = var(log_likelihood)
-    p += var_likelihood
 end
 
-#Applying the above mentioned equation to get the final WAIC value
-waic = -2*(lpppd - p);
+println("Sampling ARIMA(1, 1, 0) using NUTS...")
+chain_ARIMA110 = sample(ARIMA110(s_train), NUTS(10000, 200, 0.6) )
+
+plot(chain_ARIMA110, reuse = false, title = "Sampler plot for ARIMA(1, 1, 0) model")
+gui()
+
+@model ARIMA011(x) = begin
+    T = length(x)
+
+    # Set up error vector.
+    ϵ = Vector(undef, T)
+    x_hat = Vector(undef, T)
+
+    θ ~ Uniform(-5, 5)
+
+    # Treat the first x_hat as a parameter to estimate.
+    x_hat[1] ~ Normal(550, 220)
+    ϵ[1] = x[1] - x_hat[1]
+
+    for t in 2:T
+        # Predicted value for x.
+        x_hat[t] = x[t-1] - θ * ϵ[t-1]
+        # Calculate observed error.
+        ϵ[t] = x[t] - x_hat[t]
+        # Observe likelihood.
+        x[t] ~ Normal(x_hat[t], 1)
+    end
+end
+
+println("Sampling ARIMA(0, 0, 1) using NUTS...")
+chain_ARIMA011 = sample(ARIMA011(s_train), NUTS(10000, 1000, 0.6) )
+
+plot(chain_ARIMA011, reuse = false, title = "Sampler plot for ARIMA(0, 1, 1) model")
+gui()
+
+# ARIMA 110 AIC calculation
+
+function calculate_aic_ARIMA110(β::Float64, μ:: Float64, σ::Float64, s::Array{Float64, 1})
+    T = length(s)
+    ϵ = Vector(undef, T)
+    s_pred = Vector(undef, T)
+    
+    s_pred[1], s_pred[2] = s[1], s[2]
+    ϵ[1], ϵ[2] = 0.0, 0.0 
+    for t in 3:T
+        s_pred[t] = μ +                      
+              s_pred[t-1] +                 
+              β * (s_pred[t-1] - s_pred[t-2]) 
+        ϵ[t] = s_pred[t] - s[t]
+    end
+    log_likelihood = (-(T - 1)/2)*2*π*σ^2 - (1/σ^2)*sum(ϵ.^2) - π*σ^2/(1 - β^2) - ((s[1] - μ/(1 - β))^2)/(2*σ^2/(1 - β^2))
+    aic = -2*log_likelihood + 2
+    return aic
+end
+
+# ARIMA 011 AIC calculation
+
+function calculate_aic_ARIMA011(β::Float64, σ::Float64, s::Array{Float64, 1})
+    T = length(s)
+
+    ϵ = Vector(undef, T)
+    s_pred = Vector(undef, T)
+
+    s_pred[1] = s[1]
+    ϵ[1] = 0.0
+    for t in 2:T
+        s_pred[t] = s[t-1] - β * ϵ[t-1]
+        ϵ[t] = s[t] - s_pred[t]
+    end
+    log_likelihood = -(T/2)*log(2*π) - (T/2)*log(σ^2) + (1/2*σ^2)*sum(ϵ.^2)  
+    aic = -2*log_likelihood + 1
+    return aic
+end
+
+aicARIMA011 = calculate_aic_ARIMA011(mean(chain_ARIMA011[:θ].value), 1.0, s_train)
+println("AIC value for ARIMA011: ")
+println(aicARIMA011)
+
+aicARIMA110 = calculate_aic_ARIMA110(mean(chain_ARIMA110[:ϕ].value), mean(chain_ARIMA110[:μ].value), 1.0, s_train)
+println("AIC value for ARIMA110: ")
+println(aicARIMA110)
+
+println("Press ENTER to exit")
+read(stdin, Char)
