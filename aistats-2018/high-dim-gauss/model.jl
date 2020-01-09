4b91794f6ed27123448e0cc10d6d0bd84b83fbf9
Kai Xu <xukai921110@gmail.com>
2020-01-09 14:41:57 +0000

    improve code

--- aistats-2018/high-dim-gauss/model.jl
+++ aistats-2018/high-dim-gauss/model.jl
@@ -1,7 +1,5 @@
-using FillArrays
-
 @model high_dim_gauss(D) = begin
-    m ~ MvNormal(Fill(0, D), 1)
+    m ~ MvNormal(fill(0, D), 1)
 end
 
 get_model(args...) = high_dim_gauss(args...)
--- aistats-2018/high-dim-gauss/run.jl
+++ aistats-2018/high-dim-gauss/run.jl
@@ -13,4 +13,18 @@ Turing.setadbackend(:reverse_diff)
 
 model = get_model(data["D"])
 
-chain = sample(model, HMC(0.1, 4), 2_000, progress_style=:plain)
+alg = HMC(0.1, 4)
+n_samples = 2_000
+
+if "--benchmark" in ARGS
+    times = []
+    for i in 1:5
+        t = @elapsed chain = sample(model, alg, n_samples, progress=false)
+        push!(times, t)
+    end
+    println(times)
+else
+    chain = sample(model, alg, n_samples, progress_style=:plain)
+end
+
+;
