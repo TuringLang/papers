daadd0d9dd6e268c7c7399e6ea936d147b2ffcd9
Kai Xu <xukai921110@gmail.com>
2020-03-03 11:29:49 +0000

    use mh's hmm

--- benchmarks/hmm_semisup/turing.jl
+++ benchmarks/hmm_semisup/turing.jl
@@ -9,36 +9,16 @@ using Turing
 
 using StatsFuns: logsumexp
 
-# FIXME: tag1 + tag2 + tag3 lead to AD error
 @model hmm_semisup(K, V, T, T_unsup, w, z, u, alpha, beta, ::Type{Tv}=Vector{Float64}) where {Tv} = begin
-    theta = Vector{Tv}(undef, K)
-    for k = 1:K
-        theta[k] ~ Dirichlet(alpha)
-    end
-    # theta ~ Multi(Dirichlet(alpha), K)  # tag1
-    phi = Vector{Tv}(undef, K)
-    for k = 1:K
-        phi[k] ~ Dirichlet(beta)
-    end
-  
-    w ~ ArrayDist(Categorical.(phi[z]))
-    z[2:end] ~ ArrayDist(Categorical.(theta[z[1:end-1]]))
-    # z[2:end] ~ ArrayDist(Categorical.(theta[:,zi] for zi in z[1:end-1]))    # tag2
-
-    # Forward algorithm
-    acc, gamma, gamma′ = Tv(undef, K), Tv(undef, K), Tv(undef, K)
-    for k in 1:K
-        gamma[k] = log(phi[k][u[1]])
-    end
+    theta ~ Multi(Dirichlet(alpha), K)
+    phi ~ Multi(Dirichlet(beta), K)
+    w ~ ArrayDist(Categorical.(copy.(eachcol(phi[:, z]))))
+    z[2:end] ~ ArrayDist(Categorical.(copy.(eachcol(theta[:, z[1:end-1]]))))
+    gamma = log.(phi[u[1], 1:K])
     for t in 2:T_unsup
-        for k in 1:K
-            for j in 1:K
-              acc[j] = gamma[j] + log(theta[j][k]) + log(phi[k][u[t]])
-            #   acc[j] = gamma[j] + log(theta[k,j]) + log(phi[k][u[t]])   # tag3
-            end
-            gamma′[k] = logsumexp(acc)
+        gamma = mapreduce(vcat, 1:K) do k
+            logsumexp(gamma .+ log.(theta[k, 1:K]) .+ log(phi[u[t], k]))
         end
-        gamma .= gamma′
     end
     @logpdf() += logsumexp(gamma)
 end
