f4a33f5125b03564feece5e0a0d5ff595f9d7fb0
mohamed82008 <mohamed82008@gmail.com>
2020-04-15 15:35:29 +1000

    accelerate hmm_semisup

--- benchmarks/hmm_semisup/turing.jl
+++ benchmarks/hmm_semisup/turing.jl
@@ -9,23 +9,35 @@ include("data.jl")
 data = get_data()
 
 using Memoization, Turing
-
+using DistributionsAD: MvCategorical
 using StatsFuns: logsumexp
 
-@model hmm_semisup(K, V, T, T_unsup, w, z, u, alpha, beta, ::Type{Tv}=Vector{Float64}) where {Tv} = begin
+@model hmm_semisup(K, T_unsup, w, z, u, alpha, beta) = begin
     theta ~ filldist(Dirichlet(alpha), K)
     phi ~ filldist(Dirichlet(beta), K)
-    w ~ arraydist(Categorical.(eachcol(phi[:, z])))
-    z[2:end] ~ arraydist(Categorical.(eachcol(theta[:, z[1:end-1]])))
-    gamma = log.(phi[u[1], 1:K])
+    w ~ MvCategorical(phi[:, z])
+    z[2:end] ~ MvCategorical(theta[:, z[1:end-1]])
+
+    TF = eltype(theta)
+    acc = similar(alpha, TF, K)
+    gamma = similar(alpha, TF, K)
+    temp_gamma = similar(alpha, TF, K)
+    for k in 1:K
+        gamma[k] = log(phi[u[1],k])
+    end
     for t in 2:T_unsup
-        temp = gamma' .+ log.(theta[1:K, 1:K]) .+ log.(phi[u[t], 1:K])
-        gamma = vec(logsumexp(temp, dims=2))
+        for k in 1:K
+            for j in 1:K
+                acc[j] = gamma[j] + log(theta[k,j]) + log(phi[u[t],k])
+            end
+            temp_gamma[k] = logsumexp(acc)
+        end
+        gamma .= temp_gamma
     end
     @logpdf() += logsumexp(gamma)
 end
 
-model = hmm_semisup(data["K"], data["V"], data["T"], data["T_unsup"], data["w"], data["z"], data["u"], data["alpha"], data["beta"])
+model = hmm_semisup(data["K"], data["T_unsup"], data["w"], data["z"], data["u"], data["alpha"], data["beta"])
 
 step_size = 0.001
 n_steps = 4
